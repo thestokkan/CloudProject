03d393f7dcc840779a02d5cc64fcb9ef6562e8e2
Therese Stokkan <thestokkan@gmail.com>
2023-01-13 12:42:45 +0100

    Fixed import

--- src/main/java/com/example/cloudproject/CloudProjectApplication.java
+++ src/main/java/com/example/cloudproject/CloudProjectApplication.java
@@ -1,11 +1,12 @@
 package com.example.cloudproject;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @SpringBootApplication
